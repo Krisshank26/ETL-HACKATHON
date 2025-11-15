5407d9a5736968342abf89db8ac58c3fd6bc0a17
Krisshank26 <krisshank.24bcs10218@sst.scaler.com>
2025-11-15 21:46:00 +0530

    README.md Update

--- README.md
+++
@@ -1,8 +0,0 @@
-# etl-simulation
- 
-
-Dependencies: 
-
-npm install express
-npm install mongoose 
-npm install nodemon 
--- server/src/ingestion/extractTXT.js
+++
@@ -1,9 +0,0 @@
-// * Extract raw UTF-8 text from .txt file.
-module.exports = function extractTXT(buffer) {
-  try {
-    return buffer.toString("utf8");
-  } catch (err) {
-    console.error("TXT extraction failed:", err);
-    return "";
-  }
-};
